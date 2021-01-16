2a530551656bf3267376555c0353d49dba47a8a3
guoliangcao <guoliangcao@tencent.com>
2021-01-16 18:41:08 +0800

    add registry

--- registry/registry.go
+++ registry/registry.go
@@ -2,7 +2,7 @@ package registry
 
 import "context"
 
-// Registrar is registrar interface.
+// Registrar is service registrar interface.
 type Registrar interface {
 	Register(ctx context.Context, svc Service) error
 	Deregister(ctx context.Context, svc Service) error
@@ -10,10 +10,10 @@ type Registrar interface {
 
 // Discovery is service discovery interface.
 type Discovery interface {
-	// GetService return the services in memory according to the service name.
+	// GetService return the service instances in memory according to the service name.
 	GetService(name string) ([]Service, error)
-	// GetService return all services in memory.
-	ListServices() ([]Service, error)
+	// GetService return all service instances in memory.
+	ListService() ([]Service, error)
 	// Resolve creates a watcher according to the service name.
 	Resolve(ctx context.Context, name string) (Watcher, error)
 }
@@ -21,8 +21,8 @@ type Discovery interface {
 // Watcher is service watcher.
 type Watcher interface {
 	// Watch Return services in the following three cases:
-	// 1.the first time to watch and the service list is not empty
-	// 2.any service changes found
+	// 1.the first time to watch and the service instance list is not empty
+	// 2.any service instance changes found
 	// 3.reached the context.Deadline or conetxt.Cancel
 	// If the above three conditions are not met, it will block
 	Watch(ctx context.Context) ([]Service, error)
@@ -31,14 +31,12 @@ type Watcher interface {
 
 // Service is service interface.
 type Service interface {
-	// ID is the unique registration ID of service.
+	// ID is the unique registration ID of the service instance.
 	ID() string
 	// Name is the registration name of service.
 	Name() string
 	Version() string
 	Metadata() map[string]string
-	// Host is service hostname.
-	Host() string
 	// Endpoint schema: `grpc://127.0.0.1:8080?isSecure=false`.
 	Endpoints() []string
 }
