df0fd01b0eb1da26433b26335ec22a768581d3d9
longXboy <longxboyhi@gmail.com>
2021-01-18 19:50:10 +0800

    fix registry service

--- registry/registry.go
+++ registry/registry.go
@@ -4,16 +4,16 @@ import "context"
 
 // Registrar is service registrar interface.
 type Registrar interface {
-	Register(ctx context.Context, svc Service) error
-	Deregister(ctx context.Context, svc Service) error
+	Register(ctx context.Context, svc *Service) error
+	Deregister(ctx context.Context, svc *Service) error
 }
 
 // Discovery is service discovery interface.
 type Discovery interface {
 	// GetService return the service instances in memory according to the service name.
-	GetService(name string) ([]Service, error)
+	GetService(name string) ([]*Service, error)
 	// GetService return all service instances in memory which aggregated according to service name.
-	ListService() (map[string][]Service, error)
+	ListService() (map[string][]*Service, error)
 	// Resolve creates a watcher according to the service name.
 	Resolve(ctx context.Context, name string) (Watcher, error)
 }
@@ -24,18 +24,18 @@ type Watcher interface {
 	// 1.the first time to watch and the service instance list is not empty
 	// 2.any service instance changes found
 	// If the above two conditions are not met, it will block until context deadline exceeded or canceled
-	Watch(ctx context.Context) ([]Service, error)
+	Watch(ctx context.Context) ([]*Service, error)
 	Close()
 }
 
-// Service is service interface.
-type Service interface {
+// Service is service struct.
+type Service struct {
 	// ID is the unique registration ID of the service instance.
-	ID() string
+	ID string
 	// Name is the registration name of service.
-	Name() string
-	Version() string
-	Metadata() map[string]string
+	Name     string
+	Version  string
+	Metadata map[string]string
 	// Endpoint schema: `grpc://127.0.0.1:8080?isSecure=false`.
-	Endpoints() []string
+	Endpoints []string
 }
