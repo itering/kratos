bca397b1537e79639d49ca3ce7240a2f234adab8
chenzhihui <zhihui_chen@foxmail.com>
2021-01-25 23:32:23 +0800

    update comments

--- registry/registry.go
+++ registry/registry.go
@@ -4,7 +4,9 @@ import "context"
 
 // Registrar is service registrar interface.
 type Registrar interface {
+	// Register the registration.
 	Register(ctx context.Context, svc *Service) error
+	// Deregister the registration.
 	Deregister(ctx context.Context, svc *Service) error
 }
 
@@ -15,27 +17,33 @@ type Discovery interface {
 	// GetService return all service instances in memory which aggregated according to service name.
 	ListService() (map[string][]*Service, error)
 	// Resolve creates a watcher according to the service name.
-	Resolve(ctx context.Context, name string) (Watcher, error)
+	Resolve(name string) (Watcher, error)
 }
 
 // Watcher is service watcher.
 type Watcher interface {
-	// Watch Return services in the following two cases:
-	// 1.the first time to watch and the service instance list is not empty
-	// 2.any service instance changes found
-	// If the above two conditions are not met, it will block until context deadline exceeded or canceled
+	// Watch returns services in the following two cases:
+	// 1.the first time to watch and the service instance list is not empty.
+	// 2.any service instance changes found.
+	// if the above two conditions are not met, it will block until context deadline exceeded or canceled
 	Watch(ctx context.Context) ([]*Service, error)
+	// Close close the watcher.
 	Close()
 }
 
-// Service is service struct.
+// Service is an instance of a service in a discovery system.
 type Service struct {
-	// ID is the unique registration ID of the service instance.
+	// ID is the unique instance ID as registered.
 	ID string
-	// Name is the registration name of service.
-	Name     string
-	Version  string
+	// Name is the service name as registered.
+	Name string
+	// Version is the version of the compiled.
+	Version string
+	// Metadata is the kv pair metadata associated with the service instance.
 	Metadata map[string]string
-	// Endpoint schema: `grpc://127.0.0.1:8080?isSecure=false`.
+	// Endpoints is endpoint addresses of the service instance.
+	// schema:
+	//   http://127.0.0.1:8000?isSecure=false
+	//   grpc://127.0.0.1:9000?isSecure=false
 	Endpoints []string
 }
